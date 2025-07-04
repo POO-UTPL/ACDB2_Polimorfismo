87f70519679919faf5e7cf3e71ab31ad1f2f5942
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-07-04 11:13:36 -0500

    Uml del problema 1

--- Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
+++ Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
@@ -1,22 +1,30 @@
 public class Problema1_JuegoRoles {
+    public static void main(String[] args) {
+        
+    }
 }
 abstract class Personaje{
     public int vidas, experiencia, batallasGana;
-    public abstract boolean ataque();
+    public abstract boolean ataque(Personaje personaje);
     public abstract int defensa();
 }
 class Guerrero extends Personaje{
     public String habilidades;
-    public boolean ataque() { return false;} 
+    public boolean ataque(Personaje personaje) { 
+        //return ((int) (Math.random() * 1) == 1) ? true : false;
+        int bandera = (int) (Math.random() * 1);
+        boolean gana = (bandera == 1) ? true : false;
+        return gana;
+    } 
     public int defensa() { return 0;}
 }
 class Mago extends Personaje{
     public String estrategia;
-    public boolean ataque() { return false;} 
+    public boolean ataque(Personaje personaje) { return false;} 
     public int defensa() { return 0;}
 }
 class Arquero extends Personaje{
 public String atributo;
-    public boolean ataque() { return false;} 
+    public boolean ataque(Personaje personaje) { return false;} 
     public int defensa() { return 0;}
 }
