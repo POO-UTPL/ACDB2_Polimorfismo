35ad8b7980cdc721b92a778934e6bd02a4a734bd
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-07-04 11:03:05 -0500

    Uml del problema 1

--- Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
+++ Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
@@ -5,6 +5,18 @@ abstract class Personaje{
     public abstract boolean ataque();
     public abstract int defensa();
 }
-class Guerrero extends Personaje{}
-class Mago extends Personaje{}
-class Arquero extends Personaje{}
+class Guerrero extends Personaje{
+    public String habilidades;
+    public boolean ataque() { return false;} 
+    public int defensa() { return 0;}
+}
+class Mago extends Personaje{
+    public String estrategia;
+    public boolean ataque() { return false;} 
+    public int defensa() { return 0;}
+}
+class Arquero extends Personaje{
+public String atributo;
+    public boolean ataque() { return false;} 
+    public int defensa() { return 0;}
+}
