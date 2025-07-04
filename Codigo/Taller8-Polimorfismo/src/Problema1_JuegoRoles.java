05335874f72ed3014955d08fb1519067143e0da5
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-07-04 11:30:30 -0500

    Uml del problema 1

--- Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
+++ Codigo/Taller8-Polimorfismo/src/Problema1_JuegoRoles.java
@@ -3,8 +3,8 @@ public class Problema1_JuegoRoles {
     static Personaje mago;
     static Personaje arquero;
     public static void main(String[] args) {
-        guerrero = new Guerrero();
-        mago = new Mago();
+        guerrero = new Guerrero("Gigante", 3);
+        mago = new Mago("Invisible", 3);
         boolean gana = guerrero.ataque(mago);
         if(gana){
             guerrero.experiencia +=1;
@@ -21,6 +21,8 @@ public class Problema1_JuegoRoles {
 }
 abstract class Personaje{
     public int vidas, experiencia, batallasGana;
+    public Personaje(int vidas) {
+        this.vidas = vidas;   }
     public abstract boolean ataque(Personaje personaje);
     public abstract int defensa();
     public String toString() {
@@ -28,6 +30,9 @@ abstract class Personaje{
 }
 class Guerrero extends Personaje{
     public String habilidades;
+    public Guerrero(String habilidades, int vidas) {
+        super(vidas);
+        this.habilidades = habilidades; }
     public boolean ataque(Personaje personaje) { 
         //return ((int) (Math.random() * 1) == 1) ? true : false;
         int bandera = (int) (Math.random() * 2);
@@ -40,11 +45,17 @@ class Guerrero extends Personaje{
 }
 class Mago extends Personaje{
     public String estrategia;
+    public Mago(String estrategia, int vidas) {
+        super(vidas);
+        this.estrategia = estrategia;   }
     public boolean ataque(Personaje personaje) { return false;} 
     public int defensa() { return 0;}
 }
 class Arquero extends Personaje{
 public String atributo;
+    public Arquero(String atributo, int vidas) {
+        super(vidas);
+        this.atributo = atributo;    } 
     public boolean ataque(Personaje personaje) { return false;} 
     public int defensa() { return 0;}
 }
