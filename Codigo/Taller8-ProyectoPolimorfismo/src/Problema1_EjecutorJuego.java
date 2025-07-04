ecae3a8d192229a3f2f735b60311a55b83bcd6fd
pdirene <macbookpro@MacBookPro-pdirene.local>
2025-07-04 16:27:17 -0500

    Act

--- Codigo/Taller8-ProyectoPolimorfismo/src/Problema1_EjecutorJuego.java
+++ Codigo/Taller8-ProyectoPolimorfismo/src/Problema1_EjecutorJuego.java
@@ -3,7 +3,12 @@ public class Problema1_EjecutorJuego {
     public static Personaje mago;
     public static Personaje arquero;
     public static void main(String[] args) {
-        
+        guerrero = new Guerrero("Guigante", 3);
+        mago = new Majo("Invibilidad", 3);
+        arquero = new Arquero("Arco mas grande del mundo", 3);
+        guerrero.ataque(mago);
+        System.out.println(guerrero);
+        System.out.println(mago);
     }
     
 }
@@ -11,7 +16,7 @@ abstract class Personaje{
     public int vidas, experiencia, batallasGanadas;
     public Personaje(int vidas) {
         this.vidas = vidas; }
-    public abstract boolean ataque();
+    public abstract boolean ataque(Personaje personaje);
     public abstract int defensa();
     public String toString() {
         return "Personaje{" + "vidas=" + vidas + ", experiencia=" + experiencia + ", batallasGanadas=" + batallasGanadas + '}';}
@@ -21,10 +26,19 @@ class Guerrero extends Personaje{
     public Guerrero(String habilidades, int vidas) {
         super(vidas);
         this.habilidades = habilidades; }
-    public boolean ataque(){ 
+    public boolean ataque(Personaje personaje){ 
         //return ( ((int) Math.random() * 2) == 1) ? true : false ;
         int numero = (int) Math.random() * 2;
         boolean gana = (numero == 1) ? true : false ;
+        if (gana){
+            this.experiencia += 1;
+            this.batallasGanadas += 1;
+        }
+        else{
+            this.vidas -= 1;
+            personaje.experiencia += 1;
+            personaje.batallasGanadas += 1;
+        }
         return gana; 
     }
     public int defensa() { return 0; }
@@ -36,7 +50,7 @@ class Majo extends Personaje{
     public Majo(String estrategias, int vidas) {
         super(vidas);
         this.estrategias = estrategias;   }
-    public boolean ataque(){ return false; }
+    public boolean ataque(Personaje personaje){ return false; }
     public int defensa() { return 0; }
     public String toString() {
         return "Majo{" + "estrategias=" + estrategias + "}   " + super.toString(); }
@@ -47,7 +61,7 @@ class Arquero extends Personaje{
     public Arquero(String atributos, int vidas) {
         super(vidas);
         this.atributos = atributos;    }
-    public boolean ataque(){ return false; }
+    public boolean ataque(Personaje personaje){ return false; }
     public int defensa() { return 0; }
     public String toString() {
         return "Arquero{" + "atributos=" + atributos + "}   " + super.toString(); }
